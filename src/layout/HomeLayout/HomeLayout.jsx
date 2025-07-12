033f41f130d9642d40e01f730901f249cee58caf
shafrinahamed097 <shafrinahamedsa@gmail.com>
2025-07-12 11:33:31 -0600

    router setup And create some components

---
+++ src/layout/HomeLayout/HomeLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Navbar from "../../components/Navbar/Navbar";
+
+const HomeLayout = () => {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <Header />
+    </div>
+  );
+};
+
+export default HomeLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import HomeLayout from "./layout/HomeLayout/HomeLayout.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />
+    
+  },
+   {
+        path: '/new',
+        element: <h1>Home Layout</h1>
+      },
+      {
+        path: '/news',
+        element: <h1>News Layout</h1>
+      },
+      {
+        path: '/auth',
+        element: <h1>Login in</h1>
+      },
+      {
+        path: '*',
+        element: <h1>Error</h1>
+      } 
+  
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+   <RouterProvider router={router} />
+  </StrictMode>
+);
