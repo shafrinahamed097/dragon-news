343ec6a6e0ca73a417feab70776e508bf00c9f2d
shafrinahamed097 <shafrinahamedsa@gmail.com>
2025-07-13 12:50:37 -0600

    Design the news category

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import { useLoaderData } from "react-router-dom";
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import NewsCard from '../components/NewsCard/NewsCard';
 
 const CategoryNews = () => {
-  const { data } = useLoaderData();
-  console.log(data);
-  return <div>{data.length} New Found in this category</div>;
+    const {data: news} = useLoaderData();
+    console.log(news);
+  return (
+    <div>
+     <h2 className='font-semibold mb-3'>Dragon News Home</h2>
+     <p className='text-gray-400 text-sm'>{news.length} News Found On This Category</p>
+     <div>
+        {
+            news.map((singleNews) => <NewsCard key={singleNews._id} news={singleNews} /> )
+        }
+     </div>
+    </div>
+  );
 };
 
 export default CategoryNews;
